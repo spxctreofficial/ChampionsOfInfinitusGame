85998fa19c8623598cdf019930b574c74855e347
spxctreofficial <spxctre@spxct.cf>
2021-06-01 22:00:05 -0400

    Error Tooltips
    A working error tooltip has been implemented into the game. This has no drastic difference compared to a normal tooltip, with the exception of a red color to signify a warning.
    Typically, warning tooltips should disappear on its own, whereas a normal tooltip only disappears when a certain criteria is met, such as hiding itself whenever the mouse is no longer over a certain object.

--- Assets/Scripts/UI/Tooltip/TooltipSystem.cs
+++ Assets/Scripts/UI/Tooltip/TooltipSystem.cs
@@ -7,6 +7,7 @@ public class TooltipSystem : MonoBehaviour {
 	public static TooltipSystem instance;
 
 	public Tooltip tooltip;
+	public FixedTooltip fixedTooltip;
 
 	private void Awake() {
 		if (instance == null)
@@ -17,17 +18,30 @@ public class TooltipSystem : MonoBehaviour {
 		if (tooltip.GetComponent<CanvasGroup>() == null) tooltip.gameObject.AddComponent<CanvasGroup>();
 	}
 
-	public void Show(string body, string header = "") {
-		var gameObject = tooltip.gameObject;
-		tooltip.UpdatePosition();
-		gameObject.SetActive(true);
+	public void Show(string body, string header = "", bool error = false) {
+		switch (error) {
+			case true:
+				fixedTooltip.gameObject.SetActive(true);
+				
+				fixedTooltip.SetText(body, header);
+				fixedTooltip.GetComponent<CanvasGroup>().alpha = 0f;
+				LeanTween.alphaCanvas(fixedTooltip.GetComponent<CanvasGroup>(), 1f, 0.25f);
+				break;
+			case false:
+				tooltip.UpdatePosition();
+				tooltip.gameObject.SetActive(true);
 
-		tooltip.SetText(body, header);
-		tooltip.GetComponent<CanvasGroup>().alpha = 0f;
-		LeanTween.alphaCanvas(tooltip.GetComponent<CanvasGroup>(), 1f, 0.25f);
+				tooltip.SetText(body, header);
+				tooltip.GetComponent<CanvasGroup>().alpha = 0f;
+				LeanTween.alphaCanvas(tooltip.GetComponent<CanvasGroup>(), 1f, 0.25f);
+				break;
+		}
 	}
 	public void Hide() {
 		tooltip.GetComponent<CanvasGroup>().alpha = 0f;
 		tooltip.gameObject.SetActive(false);
+
+		fixedTooltip.GetComponent<CanvasGroup>().alpha = 0f;
+		fixedTooltip.gameObject.SetActive(false);
 	}
 }
