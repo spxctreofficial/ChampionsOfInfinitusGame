2873d3c1f8474876c901729f9f79294043cffba4
spxctreofficial <spxctre@spxct.cf>
2021-10-05 18:28:35 -0400

    Smart Hover Animation Delay Increase

--- Assets/Project/Scripts/Game/Versatile/UI/SmartHover.cs
+++ Assets/Project/Scripts/Game/Versatile/UI/SmartHover.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SmartHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
-	private Vector3 defaultScale = new Vector3(1f, 1f, 1f);
+	private readonly Vector3 defaultScale = new Vector3(1f, 1f, 1f);
+	private readonly Vector3 animatedScale = new Vector3(1.1f, 1.1f, 1.1f);
 
 	private int scaleUpID, scaleDownID;
 	
 	public void OnPointerEnter(PointerEventData eventData) {
 		AudioController.instance.Play("uiselect");
-		scaleUpID = LeanTween.scale(GetComponent<RectTransform>(), new Vector3(1.1f, 1.1f, 1.1f), 0.2f).setEaseInOutQuad().uniqueId;
+		scaleUpID = LeanTween.scale(GetComponent<RectTransform>(), animatedScale, 0.3f).setEaseInOutQuad().uniqueId;
 		LeanTween.cancel(scaleDownID);
 	}
 	public void OnPointerExit(PointerEventData eventData) {
@@ -18,7 +17,7 @@ public class SmartHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 	}
 
 	public void ScaleDown() {
-		scaleDownID = LeanTween.scale(GetComponent<RectTransform>(), defaultScale, 0.2f).setEaseInOutQuad().uniqueId;
+		scaleDownID = LeanTween.scale(GetComponent<RectTransform>(), defaultScale, 0.3f).setEaseInOutQuad().uniqueId;
 		LeanTween.cancel(scaleUpID);
 	}
 }
